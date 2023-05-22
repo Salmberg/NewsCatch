420b7fd2102da812c669c0b9e8fc8bcbf5a13e12
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-05-22 19:12:06 +0200

    Initial Commit

---
+++ NewsCatch/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NewsCatch
+//
+//  Created by David Salmberg on 2023-05-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsCatch/NewsCatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsCatchApp.swift
+//  NewsCatch
+//
+//  Created by David Salmberg on 2023-05-22.
+//
+
+import SwiftUI
+
+@main
+struct NewsCatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
