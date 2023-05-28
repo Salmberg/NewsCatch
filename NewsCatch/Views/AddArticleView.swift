8a760700422331770c1f142e965aefc8cb3f50f8
youssefazroun <yhma22.azroyo@folkuniversitetet.nu>
2023-05-28 21:19:44 +0200

    The text in the textEditor is a hint and the button in admin is clickable

--- NewsCatch/Views/AddArticleView.swift
+++ NewsCatch/Views/AddArticleView.swift
@@ -15,48 +15,65 @@ struct AddArticleView: View {
     
     //For Category label
     @State var catLabel = "Unspecified"
-        
+    
     //Needed to be able to "dismiss" the view
     @Environment(\.presentationMode) var presentation
     
     var body: some View {
-            VStack{
-                TextEditor(text: $viewModel.titleContent).padding(30).background(Color(red: 240/255, green: 240/255, blue: 245/255))
-                    .frame(height: 100)
-                TextEditor(text: $viewModel.textContent).padding(30).background(Color(red: 240/255, green: 240/255, blue: 245/255))
-                Menu { //Not very elegant, should be possible with some for-loop
-                    Button {
-                        viewModel.setCategory(cat: Category.foreign)
-                        catLabel = viewModel.categoryString
-                    } label: {
-                        Text("Foreign")
+        VStack{
+            TextEditor(text: $viewModel.titleContent)
+                .padding(30)
+                .background(Color(red: 240/255, green: 240/255, blue: 245/255))
+                .frame(height: 100)
+                .foregroundColor(viewModel.titleContent == "Enter Title..." ? .gray : .black)
+                .onTapGesture {
+                    if viewModel.titleContent == "Enter Title..." {
+                        viewModel.titleContent = ""
                     }
-                    Button {
-                        viewModel.setCategory(cat: Category.sports)
-                        catLabel = viewModel.categoryString
-                    } label: {
-                        Text("Sports")
-                    }
-                    Button {
-                        viewModel.setCategory(cat: Category.amusement)
-                        catLabel = viewModel.categoryString
-                    } label: {
-                        Text("Amusement")
-                    }
-                } label: {
-                    Image(systemName: "newspaper.circle.fill")
-                    Text(catLabel)
-                }.padding([.bottom, .trailing], 25)
-                Button("Publish", action: {viewModel.requestArticle()
-                    showingAlert = true
                 }
-                ).alert(viewModel.alertMessage, isPresented: $showingAlert) {
-                    Button("OK", role: .cancel) {
-                        self.presentation.wrappedValue.dismiss() //Dismiss the view
+            
+            TextEditor(text: $viewModel.textContent)
+                .padding(30)
+                .background(Color(red: 240/255, green: 240/255, blue: 245/255))
+                .foregroundColor(viewModel.textContent == "Enter your article text here..." ? .gray : .black)
+                .onTapGesture {
+                    if viewModel.textContent == "Enter your article text here..." {
+                        viewModel.textContent = ""
                     }
+                }
+            Menu { //Not very elegant, should be possible with some for-loop
+                Button {
+                    viewModel.setCategory(cat: Category.foreign)
+                    catLabel = viewModel.categoryString
+                } label: {
+                    Text("Foreign")
+                }
+                Button {
+                    viewModel.setCategory(cat: Category.sports)
+                    catLabel = viewModel.categoryString
+                } label: {
+                    Text("Sports")
+                }
+                Button {
+                    viewModel.setCategory(cat: Category.amusement)
+                    catLabel = viewModel.categoryString
+                } label: {
+                    Text("Amusement")
+                }
+            } label: {
+                Image(systemName: "newspaper.circle.fill")
+                Text(catLabel)
+            }.padding([.bottom, .trailing], 25)
+            Button("Publish", action: {viewModel.requestArticle()
+                showingAlert = true
+            }
+            ).alert(viewModel.alertMessage, isPresented: $showingAlert) {
+                Button("OK", role: .cancel) {
+                    self.presentation.wrappedValue.dismiss() //Dismiss the view
                 }
             }
-            .background(Color(red: 240/255, green: 240/255, blue: 245/255))
+        }
+        .background(Color(red: 240/255, green: 240/255, blue: 245/255))
         
     }
 }
