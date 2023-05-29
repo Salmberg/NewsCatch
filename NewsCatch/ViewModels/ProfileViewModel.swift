e486f637958131d84d163e769a4bbcb1d86ce871
liber09 <linda.bergsangel@hotmail.com>
2023-05-29 22:28:38 +0200

    Lots of work with layout on profile view

---
+++ NewsCatch/ViewModels/ProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewModel.swift
+//  NewsCatch
+//
+//  Created by Linda Bergsängel on 2023-05-29.
+//
+
+import Foundation
--- NewsCatch/Views/ProfileView.swift
+++ NewsCatch/Views/ProfileView.swift
@@ -15,184 +15,194 @@ struct ProfileView: View {
     var user = Auth.auth().currentUser
     var body: some View {
         NavigationView{
-            VStack{
-                HStack{
-                    Spacer()
-                    Text("Hej användarnamn")
-                        .font(.system(size: 30))
-                        .padding(.trailing, 20)
-                }
-                .background(Color.gray)
-                HStack{
-                    VStack{
-                        Image(systemName: "person.crop.circle.fill")
-                            .font(.system(size: 80))
-                            .padding(.leading, 20)
-                    
-                            Button(action: {
-                                do{
-                                    //Method for changing picture
-                                } catch let changeImageError as NSError {
-                                    print("Error changing profile image: %@", changeImageError)
-                                }
-                            }){
-                                Text("Byt bild")
-                            }
-                    }
-                    Spacer()
-                    VStack{
-                        Text("Ditt användarnamn")
-                            .font(.system(size: 20))
-                            .padding(.trailing, 20)
-                        Text("Din epost")
-                            .font(.system(size: 20))
-                            .padding(.trailing, 20)
-                        }
-                    
-                    }
-                    .background(Color.gray)
-                HStack{
-                    
-                    Button(action: {
-                        isAddArticle = true
-                    }, label: {
-                        Image(systemName: "note.text.badge.plus")
-                            .font(.system(size: 35))
-                            .padding(.leading, 25)
-                            .foregroundColor(Color.green)
-                    })
-                    .sheet(isPresented: $isAddArticle){
-                        AddArticleView()
-                    }
-                    
-                    Image(systemName: "heart.text.square.fill")
-                        .font(.system(size: 35))
-                        .padding(.leading, 25)
-                    Image(systemName: "person.text.rectangle.fill")
-                        .font(.system(size: 35))
-                        .padding(.leading, 25)
-                    Image(systemName: "calendar.badge.clock")
-                        .font(.system(size: 35))
-                        .padding(.leading, 25)
-                    
-                }
-                VStack{
-                                       
-                    NavigationLink(
-                        destination: MyFavouriteAuthors()
-                    ) {
-                        HStack {
-                            Text("Mina favorit skribenter")
-                                .font(.title)
-                                .bold()
-                                .padding(.leading, 10)
-                        }
-                    }
-                    .buttonStyle(BorderedProminentButtonStyle())
-                }
-                
-                NavigationLink(
-                    destination: MyArticles()
-                ) {
-                    HStack {
-                        Text("Mina artiklar")
-                            .font(.title)
-                            .bold()
-                            .padding(.leading, 10)
-                    }
-                }
-                .buttonStyle(BorderedProminentButtonStyle())
-                
-                
-                HStack{
-                    Button(action: {
-                        isAddArticle = true
-                    }, label: {
-                        Text("Lägg till artikel")
-                            .frame(width: 220, height: 42)
-                            .font(.title)
-                            .bold()
-                            .padding(.leading, 10)
-                            .foregroundColor(.white)
-                            .background(Color.blue)
-                            .cornerRadius(10)
-                            .buttonStyle(BorderedProminentButtonStyle())
-                    })
-                    .sheet(isPresented: $isAddArticle){
-                        AddArticleView()
-                    }
-                }
-                
-                VStack{
-                    Text("MINA ARTIKLAR")
-                        .font(.system(size: 30))
-                        .padding(.trailing, 20)
-                    VStack{
-                        Text("MINA FAVORIT SKRIBENTER")
-                            .font(.system(size: 30))
-                            .padding(.trailing, 20)
-                        HStack{
-                            Image(systemName: "person.crop.circle.fill")
-                                .font(.system(size: 80))
-                                .padding(.leading, 20)
-                            Text("Favourite author")
-                                .font(.system(size: 20))
-                                .padding(.leading, 20)
-                            Image(systemName: "person.crop.circle.fill")
-                                .font(.system(size: 80))
-                                .padding(.leading, 20)
-                            Text("Favourite author")
-                                .font(.system(size: 20))
-                                .padding(.leading, 20)
-                            Image(systemName: "person.crop.circle.fill")
-                                .font(.system(size: 80))
-                                .padding(.leading, 20)
-                            Text("Favourite author")
-                                .font(.system(size: 20))
-                                .padding(.leading, 20)
-                        }
-                    }
-                    .padding(.top, 30)
-                    VStack{
-                        Text("FAVORIT ARTIKLAR")
-                            .font(.system(size: 30))
-                            .padding(.trailing, 20)
+            ScrollView{
+                ZStack{
+                    VStack(spacing: 0){
                         HStack{
                             Spacer()
-                            NavigationLink(
-                                destination: MyFavouriteArticles()
-                            ) {
-                                HStack {
-                                    Text("Se fler")
-                                        .font(.title)
-                                        .bold()
-                                        .padding(.leading, 10)
+                            Text("Hej användarnamn")
+                                .font(.system(size: 30))
+                                .padding(.trailing, 20)
+                        }
+                        .background(Color.gray)
+                        .ignoresSafeArea()
+                        HStack(){
+                            VStack{
+                                Image(systemName: "person.crop.circle.fill")
+                                    .font(.system(size: 80))
+                                    .padding(.leading, 20)
+                                
+                                Button(action: {
+                                    do{
+                                        //Method for changing picture
+                                    } catch let changeImageError as NSError {
+                                        print("Error changing profile image: %@", changeImageError)
+                                    }
+                                }){
+                                    Text("Byt bild")
+                                        .padding(.bottom, 20)
+                                        .foregroundColor(Color.black)
                                 }
                             }
-                            .buttonStyle(BorderedProminentButtonStyle())
-                            .padding(.trailing, 15)
+                            Spacer()
+                            VStack{
+                                Text("Ditt användarnamn")
+                                    .font(.system(size: 20))
+                                    .padding(.trailing, 20)
+                                Text("Din epost")
+                                    .font(.system(size: 20))
+                                    .padding(.trailing, 20)
+                            }
+                            
                         }
+                        .background(Color.gray)
+                        HStack{
+                            
+                            Button(action: {
+                                isAddArticle = true
+                            }, label: {
+                                Image(systemName: "note.text.badge.plus")
+                                    .font(.system(size: 35))
+                                    .padding(20)
+                                    .foregroundColor(Color.white)
+                            })
+                            .sheet(isPresented: $isAddArticle){
+                                AddArticleView()
+                            }
+                            
+                            Image(systemName: "heart.text.square.fill")
+                                .font(.system(size: 35))
+                                .padding(20)
+                                .foregroundColor(Color.white)
+                            Image(systemName: "person.text.rectangle.fill")
+                                .font(.system(size: 35))
+                                .padding(20)
+                                .foregroundColor(Color.white)
+                            Image(systemName: "calendar.badge.clock")
+                                .font(.system(size: 35))
+                                .padding(20)
+                                .foregroundColor(Color.white)
+                            
+                        }
+                        .padding(20)
+                        .background(Color(red: 31/255, green:59/255,blue: 77/255))
+                        ZStack{
+                            VStack{
+                                Text("MINA ARTIKLAR")
+                                    .font(.system(size: 25))
+                                    .padding(.trailing, 20)
+                                HStack{
+                                    Spacer()
+                                    NavigationLink(
+                                        destination: MyFavouriteArticles()
+                                    ) {
+                                        HStack {
+                                            Text("Se fler")
+                                                .font(.title)
+                                                .bold()
+                                                .padding(.leading, 10)
+                                        }
+                                    }
+                                    .buttonStyle(BorderedProminentButtonStyle())
+                                    .padding(.trailing, 15)
+                                }
+                            }
+                            .background(Color.white)
+                            .cornerRadius(15)
+                            .frame(width: UIScreen.main.bounds.width * 0.9)
+                        }
+                        .padding(20)
+                        .background(Color.gray)
                         
+                        
+                        VStack{
+                            Text("MINA FAVORIT SKRIBENTER")
+                                .font(.system(size: 25))
+                                .padding(15)
+                            HStack{
+                                VStack{
+                                    Image(systemName: "person.crop.circle.fill")
+                                        .font(.system(size: 80))
+                                        .padding(15)
+                                    Text("Skribent 1")
+                                        .font(.system(size: 15))
+                                }
+                                VStack{
+                                    Image(systemName: "person.crop.circle.fill")
+                                        .font(.system(size: 80))
+                                        .padding(15)
+                                    Text("Skribent 2")
+                                        .font(.system(size: 15))
+                                }
+                                VStack{
+                                    Image(systemName: "person.crop.circle.fill")
+                                        .font(.system(size: 80))
+                                        .padding(15)
+                                    Text("Skribent 3")
+                                        .font(.system(size: 15))
+                                }
+                                
+                            }
+                            HStack{
+                                Spacer()
+                                NavigationLink(
+                                    destination: MyFavouriteAuthors()
+                                ) {
+                                    HStack {
+                                        Text("Se fler")
+                                            .font(.title)
+                                            .bold()
+                                            .padding(.leading, 10)
+                                    }
+                                }
+                                .buttonStyle(BorderedProminentButtonStyle())
+                                .padding(.trailing, 15)
+                            }
+                        }
+                        .padding(20)
+                        VStack{
+                            Text("FAVORIT ARTIKLAR")
+                                .font(.system(size: 25))
+                                .padding(.trailing, 20)
+                            HStack{
+                                Spacer()
+                                NavigationLink(
+                                    destination: MyFavouriteArticles()
+                                ) {
+                                    HStack {
+                                        Text("Se fler")
+                                            .font(.title)
+                                            .bold()
+                                            .padding(.leading, 10)
+                                    }
+                                }
+                                .buttonStyle(BorderedProminentButtonStyle())
+                                .padding(.trailing, 15)
+                            }
+                            
+                        }
+                        .padding(20)
+                        
+                        
+                        
+                        
+                        Spacer()
+                        
+                        Spacer()
+                        Button(action: {
+                            do{
+                                try Auth.auth().signOut()
+                            } catch let signOutError as NSError {
+                                print("Error signing out: %@", signOutError)
+                            }
+                        }){
+                            Text("Logout")
+                        }
                     }
-                }
-                
-                
-                
-                Spacer()
-                
-                Spacer()
-                Button(action: {
-                    do{
-                        try Auth.auth().signOut()
-                    } catch let signOutError as NSError {
-                        print("Error signing out: %@", signOutError)
-                    }
-                }){
-                    Text("Logout")
                 }
             }
+            .background(Color.gray)
         }
-        
     }
 }
 
