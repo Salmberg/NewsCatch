c21172c5c12a52616704eb8e65fbfabe93a5620b
liber09 <linda.bergsangel@hotmail.com>
2023-06-05 16:34:11 +0200

    We can add markers to the map

--- NewsCatch/Views/MapView.swift
+++ NewsCatch/Views/MapView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 import MapKit
 
+struct ArticleLocation: Identifiable{
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
 struct MapView: View {
     @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 57.7087, longitude:11.9751), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    let locations = [
+        ArticleLocation(name: "Liseberg", coordinate: CLLocationCoordinate2D(latitude: 57.6968, longitude: 11.99)),
+        ArticleLocation(name: "Slotsskogen", coordinate: CLLocationCoordinate2D(latitude: 57.6863684, longitude: 11.942684))
+    ]
+    
     var body: some View {
-        Map(coordinateRegion: $mapRegion)
+        Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+            MapMarker(coordinate: location.coordinate)
+        }
     }
 }
+
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
