91ffe629617cd520283133edf4fe96d9db0f2b45
liber09 <linda.bergsangel@hotmail.com>
2023-06-05 15:37:23 +0200

    The map zooms in on Gothenburg

--- NewsCatch/Views/MapView.swift
+++ NewsCatch/Views/MapView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 57.7087, longitude:11.9751), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map(coordinateRegion: $mapRegion)
     }
 }
 
