599521d7b529658a551c0e8beaefb76a310f89e4
Viktorjb <115181183+Viktorjb@users.noreply.github.com>
2023-06-07 00:54:02 +0200

    follow author fix

--- NewsCatch/ViewModels/MyFavouriteAuthorsViewModel.swift
+++ NewsCatch/ViewModels/MyFavouriteAuthorsViewModel.swift
@@ -43,6 +43,36 @@ class MyFavouriteAuthorViewModel: ObservableObject{
       
     }
     
+    /*func saveFavouriteWriter(userName: String){
+        guard let uid = Auth.auth().currentUser?.uid else {return}
+        let allUsers = [User]()
+        
+        db.collection("users").addSnapshotListener() {
+            snapshot, error in
+            
+            guard let snapshot = snapshot else {return}
+            if let error = error {
+                print("Error listning to FireStore \(error)")
+            }else{
+                for document in snapshot.documents{
+                    do{
+                        let dbUser = try document.data(as: User.self)
+                        if(dbUser.username == userName){
+                            allUsers.append(dbUser)
+                        }
+                    }
+                }catch{
+                    print("Error reading from FireStore")
+                }
+                
+        // Upload to Firebase
+        do {
+            try db.collection("users").document(uid).collection("favoriteWriters").addDocument(from: newArticle)
+        } catch {
+            print("Error sending to Database")
+        }
+    }*/
+    
     
     func saveFavouriteAuthor(userName: String){
         
@@ -50,7 +80,7 @@ class MyFavouriteAuthorViewModel: ObservableObject{
         var authUser = Auth.auth().currentUser
         users.removeAll()
         
-        db.collection("writers").document(uid).collection("favouriteWriters").addSnapshotListener() {
+        db.collection("users").addSnapshotListener() {
                 snapshot, error in
                 
                 guard let snapshot = snapshot else {return}
@@ -62,53 +92,56 @@ class MyFavouriteAuthorViewModel: ObservableObject{
                             let dbUser = try document.data(as: User.self)
                             if(dbUser.username == userName){
                                 print(dbUser)
-                                self.users.append(dbUser)
+                                self.users.append(dbUser) //append the writer, happens eventually
                             }
                         }catch{
                             print("Error reading from FireStore")
                         }
                     }
                     
+                    
+                    let favouriteAuthorsCollection = self.db.collection("authors")
+                        .document(uid)
+                        .collection("favouriteWriters")
+                    
+                    let savedArticlesQuery = favouriteAuthorsCollection
+                        .whereField("email", isEqualTo: authUser?.email)
+                        
+                        savedArticlesQuery.getDocuments { snapshot, error in
+                            if let error = error {
+                                print("Error retrieving saved articles: \(error)")
+                                return
+                            }
+                            
+                            if snapshot?.isEmpty == false {
+                              
+                                print("Author is already saved!")
+                            } else {
+                                
+                                if(!self.users.isEmpty){
+                                    var data: [String: Any] = [
+                                        "username": self.users[0].username, //out of bounds fatal error
+                                        "image": self.users[0].imageURL,
+                                        "name": self.users[0].name,
+                                        "email": self.users[0].email
+                                    ]
+                                    print("I ACTUALLY HAPPENED")
+                                    
+                                    
+                                    favouriteAuthorsCollection.addDocument(data: data) { error in
+                                        if let error = error {
+                                            print("Error saving author: \(error)")
+                                        } else {
+                                            print("Author saved successfully!")
+                                        }
+                                    }
+                                }
+                            }
+                    
                 }
             }
 
-        let favouriteAuthors = db.collection("writers")
-                .document(uid)
-                .collection("favouriteWriters")
         
-        let favouriteAuthorsCollection = db.collection("authors")
-            .document(uid)
-            .collection("favouriteWriters")
-        
-        let savedArticlesQuery = favouriteAuthorsCollection
-            .whereField("email", isEqualTo: authUser?.email)
-            
-            savedArticlesQuery.getDocuments { snapshot, error in
-                if let error = error {
-                    print("Error retrieving saved articles: \(error)")
-                    return
-                }
-                
-                if snapshot?.isEmpty == false {
-                  
-                    print("Author is already saved!")
-                } else {
-            
-                    var data: [String: Any] = [
-                        "username": self.users[0].username,
-                        "image": self.users[0].imageURL,
-                        "name": self.users[0].name,
-                        "email": self.users[0].email
-                    ]
-                    
-                    favouriteAuthorsCollection.addDocument(data: data) { error in
-                        if let error = error {
-                            print("Error saving author: \(error)")
-                        } else {
-                            print("Author saved successfully!")
-                        }
-                    }
-                }
             }
         }
         
