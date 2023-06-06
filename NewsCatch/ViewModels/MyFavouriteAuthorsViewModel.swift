36b884e470015db470de208780800c7bdc57bd4a
Viktorjb <115181183+Viktorjb@users.noreply.github.com>
2023-06-07 01:18:41 +0200

    hopefully working favWriter

--- NewsCatch/ViewModels/MyFavouriteAuthorsViewModel.swift
+++ NewsCatch/ViewModels/MyFavouriteAuthorsViewModel.swift
@@ -21,57 +21,32 @@ class MyFavouriteAuthorViewModel: ObservableObject{
             return
         }
         
-        let favouriteAuthorsCollection = db.collection("writers")
+        let favouriteAuthorsCollection = db.collection("authors")
             .document(uid)
             .collection("favouriteWriters")
         
-        favouriteAuthorsCollection.getDocuments { snapshot, error in
+        favouriteAuthorsCollection.addSnapshotListener() {
+            snapshot, error in
+            
+            guard let snapshot = snapshot else {return}
+            
             if let error = error {
                 print("Error retrieving favourite authors: \(error)")
                 return
-            }
-            do{
-                for document in snapshot?.documents ?? [] {
-                    let user = try document.data(as: User.self)
-                    self.MyAuthors.append(user)
+            } else{
+                for document in snapshot.documents{
+                    do{
+                        let dbUser = try document.data(as: User.self)
+                        self.MyAuthors.append(dbUser)
+                    }catch{
+                        print("Error reading from FireStore")
+                    }
                 }
-            }catch{
-                print("Error reading from FireStore")
             }
-
         }
       
     }
     
-    /*func saveFavouriteWriter(userName: String){
-        guard let uid = Auth.auth().currentUser?.uid else {return}
-        let allUsers = [User]()
-        
-        db.collection("users").addSnapshotListener() {
-            snapshot, error in
-            
-            guard let snapshot = snapshot else {return}
-            if let error = error {
-                print("Error listning to FireStore \(error)")
-            }else{
-                for document in snapshot.documents{
-                    do{
-                        let dbUser = try document.data(as: User.self)
-                        if(dbUser.username == userName){
-                            allUsers.append(dbUser)
-                        }
-                    }
-                }catch{
-                    print("Error reading from FireStore")
-                }
-                
-        // Upload to Firebase
-        do {
-            try db.collection("users").document(uid).collection("favoriteWriters").addDocument(from: newArticle)
-        } catch {
-            print("Error sending to Database")
-        }
-    }*/
     
     
     func saveFavouriteAuthor(userName: String){
@@ -92,7 +67,7 @@ class MyFavouriteAuthorViewModel: ObservableObject{
                             let dbUser = try document.data(as: User.self)
                             if(dbUser.username == userName){
                                 print(dbUser)
-                                self.users.append(dbUser) //append the writer, happens eventually
+                                self.users.append(dbUser)
                             }
                         }catch{
                             print("Error reading from FireStore")
@@ -105,7 +80,7 @@ class MyFavouriteAuthorViewModel: ObservableObject{
                         .collection("favouriteWriters")
                     
                     let savedArticlesQuery = favouriteAuthorsCollection
-                        .whereField("email", isEqualTo: authUser?.email)
+                        .whereField("email", isEqualTo: self.users[0].email)
                         
                         savedArticlesQuery.getDocuments { snapshot, error in
                             if let error = error {
@@ -121,11 +96,12 @@ class MyFavouriteAuthorViewModel: ObservableObject{
                                 if(!self.users.isEmpty){
                                     var data: [String: Any] = [
                                         "username": self.users[0].username, //out of bounds fatal error
-                                        "image": self.users[0].imageURL,
+                                        "imageURL": self.users[0].imageURL,
                                         "name": self.users[0].name,
-                                        "email": self.users[0].email
+                                        "email": self.users[0].email,
+                                        "joined": self.users[0].joined,
+                                        "id": self.users[0].id
                                     ]
-                                    print("I ACTUALLY HAPPENED")
                                     
                                     
                                     favouriteAuthorsCollection.addDocument(data: data) { error in
