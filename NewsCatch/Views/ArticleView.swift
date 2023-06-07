702497c7954a55bae8661daa0dc332443a8404b2
youssefazroun <yhma22.azroyo@folkuniversitetet.nu>
2023-06-07 17:39:41 +0200

    Fix for back stack buttons 2

--- NewsCatch/Views/ArticleView.swift
+++ NewsCatch/Views/ArticleView.swift
@@ -16,14 +16,13 @@ struct ArticleView: View {
     @State private var showAlert = false
     @State private var isShowingWriterSheet = false
     
-    
     var body: some View {
         VStack {
             ScrollView {
                 Text(article.heading)
                     .font(.custom("BebasNeue-Regular", size: 30))
                     .padding()
-                                    
+                
                 if let pictureURL = article.pictureURL {
                     KFImage(URL(string: pictureURL))
                         .resizable()
@@ -36,18 +35,19 @@ struct ArticleView: View {
                 Text(article.content)
                     .padding()
                     .font(.custom("CrimsonText-Regular", size: 18))
-               
+                
                 Spacer()
-                     
-                HStack{
+                
+                HStack {
                     Image(systemName: "calendar")
                     Text(Article.dateFormatter.string(from: article.date))
                         .padding()
                 }
-                HStack{
-                    NavigationLink(
-                        destination: WriterArticlesView(writer: self.article.writer)
-                    ) {
+                
+                HStack {
+                    Button(action: {
+                        isShowingWriterSheet = true
+                    }) {
                         HStack {
                             Image(systemName: "person")
                             Text(article.writer)
@@ -57,14 +57,13 @@ struct ArticleView: View {
                     .buttonStyle(PlainButtonStyle())
                     .padding()
                 }
-
+                
                 Button(action: {
                     viewModel.saveArticle(article)
                     showAlert = true
                 }) {
                     HStack {
                         Image(systemName: "bookmark")
-                            
                     }
                 }
                 .alert(isPresented: $showAlert) {
@@ -72,8 +71,6 @@ struct ArticleView: View {
                         title: Text("Article saved!"),
                         message: Text("The article is now under saved articles in your profile"),
                         dismissButton: .default(Text("OK"))
-                    
-                        
                     )
                 }
                 .padding()
@@ -89,16 +86,19 @@ struct ArticleView: View {
         }
         .navigationBarBackButtonHidden(true)
         .navigationBarItems(leading:
-                                Button(action: {
-            presentationMode.wrappedValue.dismiss()
-        }) {
-            Image(systemName: "chevron.left")
-                .foregroundColor(.black)
-        }
+            Button(action: {
+                presentationMode.wrappedValue.dismiss()
+            }) {
+                Image(systemName: "chevron.left")
+                    .foregroundColor(.black)
+            }
         )
+        .sheet(isPresented: $isShowingWriterSheet) {
+            WriterArticlesView(writer: article.writer)
+        }
     }
-    
 }
+
 
 //struct ArticleView_Previews: PreviewProvider {
 //    static var previews: some View {
